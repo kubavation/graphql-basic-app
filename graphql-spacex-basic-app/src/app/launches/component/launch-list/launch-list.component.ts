fcf20b7c5578678b322a5437179de50307b11a13
kubavation <kubavation@wp.pl>
2019-11-03 13:10:22 +0100

    basic launches-list view

--- graphql-spacex-basic-app/src/app/launches/component/launch-list/launch-list.component.ts
+++ graphql-spacex-basic-app/src/app/launches/component/launch-list/launch-list.component.ts
@@ -1,4 +1,7 @@
+import { LaunchesListGQL } from './../../../services/spacexGraphql.service';
 import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
+import { Observable } from 'apollo-link';
 
 @Component({
   selector: 'app-launch-list',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LaunchListComponent implements OnInit {
 
-  constructor() { }
+  private launches$;
+
+  constructor(private launchesService: LaunchesListGQL) { }
 
   ngOnInit() {
+    this.launches$ = this.launchesService
+      .fetch({limit: 20})
+      .pipe(
+        map(res => res.data.launchesPast)
+      );
   }
 
 }
