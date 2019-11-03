537b9a2ea257228d4e559dcda5927be5b36d000a
kubavation <kubavation@wp.pl>
2019-11-03 12:31:42 +0100

    some codegen config + generated data

--- graphql-spacex-basic-app/src/app/app.module.ts
+++ graphql-spacex-basic-app/src/app/app.module.ts
@@ -5,6 +5,8 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { GraphQLModule } from './graphql.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    GraphQLModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
